b2dee78f0c4cec0054c63a9e19ffe123165a666b
ericklarsen <ericklarsenn@gmail.com>
2020-09-13 02:21:53 +0700

    upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import gambar from './assets/gambar.jpg'
+
+function App() {
+    return (
+        <div>
+            
+            <h1>Halo World</h1>
+            <img src={gambar} alt="gambar"/>
+        </div>
+    )
+}
+
+export default App
